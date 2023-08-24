6c3b8a7eb0c1161d586d02fc7e97977f4bc27478
Charleston A <charleston.anjos@gmail.com>
2023-08-24 12:58:11 -0300

    sample

---
+++ sample/index.js
@@ -0,0 +1,12 @@
+const { WooCommerceApi } = require("./../dist/index.js");
+
+const wc = WooCommerceApi({
+  url: "http://localhost/wp-json",
+  consumerKey: "ck_7644a791d1ba617a48f49a429647a4307f15d989",
+  consumerSecret: "cs_51162e49ea1ff02f31bae622ce90a5e52b2cd06d",
+  version: "wc/v3",
+});
+
+wc.get("orders").then((orders) => {
+  console.log(orders);
+});
